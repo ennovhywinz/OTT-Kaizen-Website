97b8034a5d0428d197ff5f245b9f8daf4042b98c
Chaewonblight <tsukigrayskull223@gmail.com>
2025-02-01 05:59:59 +0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -15,7 +15,7 @@
         
         const pipButton = document.createElement('button');
         pipButton.classList.add('shaka-pip-button');
-        pipButton.innerHTML = 'PiP';
+        pipButton.innerHTML = '<img src="https://i.imgur.com/sbtYsFs.png">';
         pipButton.style.cssText = `
             position: absolute;
             top: 10px;

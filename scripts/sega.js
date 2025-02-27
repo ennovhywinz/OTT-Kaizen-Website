4d0f555df09cc7585beae586bef3c3aea2712dc4
ennovhywinz <51473650+ennovhywinz@users.noreply.github.com>
2025-02-27 18:54:07 +0800

    Update sega.js

--- scripts/sega.js
+++ scripts/sega.js
@@ -1,4 +1,8 @@
 function SetDa(a) {
                 a.href = "https://imgur.com/a/V4hpORC";
             };
+function SetDest(a) {
+                a.href = "https://facebook.com/eldersky";
+            };
+            
             
